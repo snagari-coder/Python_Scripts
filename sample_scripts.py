dbd212e5cd59c717eee928403849f5dff8f0b9b4
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2021-07-22 11:21:52 -0700

    Update sample_scripts.py

--- sample_scripts.py
+++ sample_scripts.py
@@ -22,3 +22,39 @@ src_str = re.compile("this", re.IGNORECASE)  # Means ignore case of "this"
 str_replaced = src_str.sub("that",
                            "This is a test sentence. this is a test sentence. THIS")  # replace 'that' with 'this'
 print(str_replaced)
+
+
+# Script to read a file with contents as shown below, and find the total cost of expenditure by comp1:
+# comp1 200
+# comp2 100
+# comp3 300
+# comp1 200
+cost = 0
+with open('input_file.txt') as text_file:
+	for line in text_file:
+		first_word = line.split(' ', 1)[0] #splits the words in a line delimited by space, makes 1 split, takes first element of the list
+		if first_word == 'comp1':
+			cost += int(line.split(' ', 1)[1])
+		
+		print(line, end='')
+	print(cost)
+    
+ 
+#script to read a file with contents as shown below, and find the total occurance of the word "comp1":
+# comp1 44 comp1
+# comp2 500 comp2
+# comp3 600
+# comp2 150
+# comp1 350 comp1
+
+count = 0
+with open('input_file.txt') as text_file:
+	for line in text_file:
+		line = line.rstrip('\n')  # strips each file of the new line char '\n'
+		word_list = line.split(' ')
+		for i in range(len(word_list)):
+			
+			if word_list[i] == 'comp1':
+				count += 1
+		
+	print(count)
